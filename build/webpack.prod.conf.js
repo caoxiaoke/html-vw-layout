d1b016f5896be1b9cf67e54355a4f9dcdb72c56b
曹小科 <364549027@qq.com>
2018-12-12 16:49:46 +0800

    Update webpack.prod.conf.js

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,6 +1,5 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const config = require('./conf.js');
 const webpackConfig = require('./webpack.conf.js');
 const OptimizeCSSPlugin  = require("optimize-css-assets-webpack-plugin");
@@ -100,13 +99,11 @@ const webpackProdConfig = {
 		  }
 	  ]
   },
-  // devtool: '#source-map',
 	devtool: 'false'
 
 }
 
 let plugins = [
-  // new ExtractTextPlugin({ filename: "styles.[chunkhash].css", allChunks: true }),
 	new MiniCssExtractPlugin({
 		filename: "[name].[chunkhash].css",
 		chunkFilename: "[id].[chunkhash].css"
@@ -116,15 +113,6 @@ let plugins = [
 			safe: true
 		}
 	})
-	// new webpack.optimize.UglifyJsPlugin({
-  //   compress: {
-  //     warnings: false
-  //   }
-  // }),
-  // new webpack.optimize.CommonsChunkPlugin({
-  //   name: 'common', // 将公共模块提取,生成名为`common`的chunk
-  //   minChunks: 2 // 提取至少2个模块共有的部分
-  // })
 ]
 webpackConfig.plugins.push.apply(webpackConfig.plugins, plugins);
 module.exports = merge(webpackConfig, webpackProdConfig);
